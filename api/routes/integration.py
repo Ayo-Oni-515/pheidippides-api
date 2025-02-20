ea6c056961b29d4452aaa1c364d3ceaadea3f06d
ayodeji oni <ayodejioni1505@gmail.com>
2025-02-20 15:53:29 +0100

    feat/added tick url

--- api/routes/integration.py
+++ api/routes/integration.py
@@ -6,3 +6,8 @@ router = APIRouter()
 @router.get("/integration")
 def integration_json():
     return {"integration status": "Healthy"}
+
+
+@router.post("/tick")
+def tick_url():
+    return {"tick url status": "Healthy"}
