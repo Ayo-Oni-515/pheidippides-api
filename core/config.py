82a7b51010cfe5beafa3c97f6ed7536691249fd1
ayodeji oni <ayodejioni1505@gmail.com>
2025-02-21 14:43:13 +0100

    chore: correct PROJECT_DESCRIPTION value

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Pheidippides API"
     PROJECT_VERSION: str = "0.1.0"
-    PROJECT_DESCRIPTION: str = "An integration built for telex"
+    PROJECT_DESCRIPTION: str = "An integration that suggests books to read based on any selected genre."
     API_PREFIX: str = "/pheidippides-api"
     
 
