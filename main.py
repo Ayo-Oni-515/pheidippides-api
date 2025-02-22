4eb67f7547c6f5b43679bfcf8ca8d2def9c75333
ayodeji oni <ayodejioni1505@gmail.com>
2025-02-22 16:00:45 +0100

    refator: improved speed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import httpx
 
 from api.router import api_router
 from core.config import settings
@@ -23,14 +21,6 @@ app.add_middleware(
 app.include_router(api_router, prefix=settings.API_PREFIX)
 
 
-class Payload(BaseModel):
-    event_name: str = "string"
-    message: str = "python post"
-    status: str = "success"
-    username: str = "Ayodeji"
-
-telex_channel_url = "https://ping.telex.im/v1/webhooks/0195057a-ebc9-7646-af52-41800fa80490"
-
 @app.get("/")
 async def root():
     return {
@@ -39,31 +29,3 @@ async def root():
         "description": "An integration that suggests books to read based on any selected genre.",
         "version": "0.1.0",
     }
-
-async def telex(payload_data: Payload):
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            telex_channel_url,
-            json=payload_data,
-            headers={
-                "Accept" : "application/json",
-                "Content-Type" : "application/json"
-                }
-        )
-
-        return response
-
-
-# @app.post("/pheidippedes")
-# async def test_webhook(post_payload: Payload):
-#     return {"message": "Webhook sent successfully"}
-
-@app.post("/pheidippedes")
-async def test_webhook(post_payload: Payload):
-    try:
-        response = await telex(post_payload)
-        return {"message": "Webhook sent successfully", "response" : response}
-    except Exception as e:
-        return {"message": "Webhook failed", "error": e}
-
-
